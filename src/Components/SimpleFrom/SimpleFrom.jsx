8e558e28f2a5b0746497da3d8db3bd16fa8cdf39
asifaktar <asifaktar7767@gmail.com>
2025-01-10 19:33:57 +0530

    added some cheange

--- src/Components/SimpleFrom/SimpleFrom.jsx
+++ src/Components/SimpleFrom/SimpleFrom.jsx
@@ -1,16 +1,25 @@
 
+import { useRef } from "react";
 import "./SimpleForm.css"
+import { useEffect } from "react";
 const SimpleFrom = () => {
     const handleSubmit = (e) =>{
-        e.preventDefault()
+        e.preventDefault();
         console.log("Submit the form!");
     }
+
+    const nameRef = useRef(null);
+            useEffect(() =>{
+                nameRef.current.focus();
+            }, []);
+   
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div className="SimpleForm">
                     <h2>Bissmillah hirrohamaner rahim</h2>
-                    <input className="input1" placeholder="Enter text" type="text" />
+                    <input ref={nameRef} className="input1" name="name" placeholder="Enter text" type="text" />
                     <br />
                     <input className="input2" type="submit" name="submit" />
                 </div>
